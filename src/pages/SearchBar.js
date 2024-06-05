339afe46afd8f3923bd64d83acf130936a40cd7b
Sushmitha Rajendran <rajendran.sushmitha@in.bosch.com>
2024-06-05 16:19:02 +0530

    Select default as German

--- src/pages/SearchBar.js
+++ src/pages/SearchBar.js
@@ -1,34 +1,32 @@
-import React, { useState } from 'react';
-import { IconButton,  MenuItem, Select } from '@mui/material';
-import { useTranslation } from 'react-i18next';
+import React, { useState } from "react";
+import { IconButton, MenuItem, Select } from "@mui/material";
+import { useTranslation } from "react-i18next";
 const LanguageBar = () => {
-  const [selectedLanguage, setSelectedLanguage] = useState("en"); 
+  const [selectedLanguage, setSelectedLanguage] = useState("en");
   const { i18n } = useTranslation();
-  
+
   const handleLanguageChange = (event) => {
     const newLanguage = event.target.value;
     setSelectedLanguage(newLanguage);
     i18n.changeLanguage(newLanguage);
-    
   };
-  
+
   return (
     <div>
-    <div className="language-bar">
-      <Select
-        value={selectedLanguage}
-        onChange={handleLanguageChange}
-        variant="standard"
-        autoWidth
-      >
-        <MenuItem value="en">English</MenuItem>
-        <MenuItem value="de">German</MenuItem>
-      </Select>
-       <IconButton> 
-      </IconButton>
-      
+      <div className="language-bar">
+        <Select
+          value={selectedLanguage}
+          onChange={handleLanguageChange}
+          variant="standard"
+          autoWidth
+          defaultValue="de"
+        >
+          <MenuItem value="en">English</MenuItem>
+          <MenuItem value="de">German</MenuItem>
+        </Select>
+        <IconButton></IconButton>
+      </div>
     </div>
-   </div>
   );
 };
 
